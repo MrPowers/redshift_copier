099a7f0ef89c473f7735fe1a010204947115f200
MrPowers <matthewkevinpowers@gmail.com>
2016-03-03 14:54:28 -0500

    Add a copy_file class to copy a single file from S3 into Redshift

--- lib/redshift_copier.rb
+++ lib/redshift_copier.rb
@@ -1,5 +1,13 @@
 require "redshift_copier/version"
 
+require 'pg'
+
+def require_all(pattern)
+  root = File.expand_path("../", File.dirname(__FILE__))
+  Dir.glob("#{root}/#{pattern}/**/*.rb").sort.each { |path| require path }
+end
+
+require_all("lib/redshift_copier")
+
 module RedshiftCopier
-  # Your code goes here...
 end
---
+++ lib/redshift_copier/copy_file.rb
@@ -0,0 +1,55 @@
+module RedshiftCopier; class CopyFile
+
+  attr_reader :schema, :table, :create_sql, :aws_access_key_id, :aws_secret_access_key, :s3_csv_location, :db_config, :copy_command_options
+
+  def initialize(args)
+    @schema = args.fetch(:schema)
+    @table = args.fetch(:table)
+    @create_sql = args.fetch(:create_sql)
+    @aws_access_key_id = args.fetch(:aws_access_key_id)
+    @aws_secret_access_key = args.fetch(:aws_secret_access_key)
+    @s3_csv_location = args.fetch(:s3_csv_location)
+    @db_config = args.fetch(:db_config)
+    @copy_command_options = args.fetch(:copy_command_options, "dateformat 'auto' timeformat 'auto' blanksasnull emptyasnull escape removequotes delimiter ',' ignoreheader 1;")
+  end
+
+  def run
+    drop
+    create
+    copy
+  end
+
+  private
+
+  def drop
+    connection.exec(drop_sql)
+  end
+
+  def drop_sql
+    "drop table if exists #{schema}.#{table};"
+  end
+
+  def create
+    connection.exec(create_sql)
+  end
+
+  def copy_sql
+    "copy #{schema}.#{table} from '#{s3_csv_location}'"\
+    " credentials '#{credentials}'"\
+    " #{copy_command_options}"
+  end
+
+  def credentials
+    "aws_access_key_id=#{aws_access_key_id};aws_secret_access_key=#{aws_secret_access_key}"
+  end
+
+  def copy
+    connection.exec(copy_sql)
+  end
+
+  def connection
+    PGconn.connect(db_config)
+  end
+
+end; end
+
